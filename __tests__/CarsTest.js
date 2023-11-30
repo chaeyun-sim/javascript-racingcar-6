1182d10e73ae874ceec7a82ce97abe545dec3a0d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-30 16:09:01 +0900

    ✅ test: Cars 클래스에 대한 테스트 구현

---
+++ __tests__/CarsTest.js
@@ -0,0 +1,36 @@
+import { ERROR_TEXT } from '../src/constants/constants'
+import Cars from '../src/Model/Cars'
+
+describe('Cars 클래스에 대한 테스트', () => {
+  test.each([
+    '',
+    ,
+  ])('빈 문자열이 입력되었을 때 예외를 발생시킨다.', input => {
+    expect(() => new Cars(input).returnCars()).toThrow(ERROR_TEXT.emptyInput)
+  });
+
+  test('입력된 값이 쉼표(',')로 구분되나 빈 칸일 때 예외를 발생시킨다.', () => {
+    const input = ',,,,'
+    expect(() => new Cars(input).returnCars()).toThrow(ERROR_TEXT.emptyInput)
+  })
+
+  test.each([
+    'john,daaaaaaaavid',
+    'maximm'
+  ])('입력된 값이 5자보다 많다면 예외를 발생시킨다.', input => {
+    expect(() => new Cars(input).returnCars()).toThrow(ERROR_TEXT.overNameMax)
+  });
+
+  test.each([
+    'john,jun!',
+    '1,2,3_',
+  ])('입력된 값이 한글, 영문, 숫자 외의 문자가 입력되었다면 예외를 발생시킨다', input=> {
+    expect(() => new Cars(input).returnCars()).toThrow(ERROR_TEXT.onlyChar)
+  })
+
+  test.each([
+    'test,test,test',
+  ])('중복된 이름이 있다면 예외를 발생시킨다.', input => {
+    expect(() => new Cars(input).returnCars()).toThrow(ERROR_TEXT.duplicated)
+  })
+})
