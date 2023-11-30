d9ebfe1227c85ce4ab2f012e16d33615c673f5c5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-30 13:56:20 +0900

    📦 chore: 에러 메세지와 매직넘버 관리를 위한 constants 추가

---
+++ src/constants/constants.js
@@ -0,0 +1,24 @@
+const NAME_MAXIMUM = 5;
+const LAP_MAXIMUM = 100000;
+const GO_FORWARD = 4;
+const STOP = 3;
+
+
+const GUIDE_TEXT = Object.freeze({
+  insertCarNames: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  insertLaps: '시도할 횟수는 몇 회인가요?',
+  printResult: '실행 결과',
+  printWinner: '최종 우승자 : '
+});
+
+const ERROR_TEXT = Object.freeze({
+  emptyInput: '[ERROR] 잘못된 입력입니다. 빈 문자열은 입력할 수 없습니다.',
+  correctSeperation: '[ERROR] 잘못된 형식입니다. 여러 대의 이름은 쉼표(,)로 구분하여 입력해야합니다.',
+});
+
+const PATTERNS = Object.freeze({
+	number: /^[0-9]+$/,
+	onlyChar: /^[a-zA-Z0-9가-힣]+$/,
+});;
+
+export {NAME_MAXIMUM, LAP_MAXIMUM, GO_FORWARD, STOP, GUIDE_TEXT, ERROR_TEXT, PATTERNS};
